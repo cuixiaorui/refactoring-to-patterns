ed3d55b41d1be81c4a05d2aa783959573736655d
cuixiaorui <cui_xiaorui@126.com>
2020-03-30 21:02:08 +0800

    fix: DOMBuilder change to XMLBuilder

--- Factory 6.4/normal/XMLBuilderTest.ts
+++ Factory 6.4/normal/XMLBuilderTest.ts
@@ -1,12 +1,12 @@
 import TestCase from "./TestCase";
 import OutputBuild from "./IOutputBuilder";
-import DOMBuilder from "./DOMBuilder";
+import XMLBuilder from "./XMLBuilder";
 
 export default class XMLBuilderTest extends TestCase {
   private builder: OutputBuild;
   public testAddAboveRoot() {
 
-    this.builder = new DOMBuilder("orders");
+    this.builder = new XMLBuilder("orders");
     this.builder.addBelow("order");
 
     try {
